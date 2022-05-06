49a41c2e3d6d254411aac90f35187b89d569b87f
kaunovka <kaunovka@gmail.com>
2022-05-06 13:04:01 +0300

    Format code with black

--- src/forest_prediction/generate_eda.py
+++ src/forest_prediction/generate_eda.py
@@ -4,20 +4,21 @@ import pandas as pd
 from pandas_profiling import ProfileReport
 import click
 
+
 @click.command()
 @click.option(
     "-d",
     "--dataset-path",
     default="data/train.csv",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 @click.option(
     "-o",
     "--output-path",
     default="data/report.html",
-    type=click.Path(dir_okay=True, path_type=Path)
+    type=click.Path(dir_okay=True, path_type=Path),
 )
-def generate(dataset_path : Path, output_path: Path) -> None:
+def generate(dataset_path: Path, output_path: Path) -> None:
     data = pd.read_csv(dataset_path)
     report = ProfileReport(data)
     report.to_file(output_path)
--- src/forest_prediction/train.py
+++ src/forest_prediction/train.py
@@ -12,13 +12,14 @@ from sklearn.model_selection import cross_validate
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
+
 @click.command()
 @click.option(
     "-d",
     "--dataset-path",
-    default="data/train.csv",  
+    default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
-    show_default=True
+    show_default=True,
 )
 @click.option(
     "-s",
@@ -30,8 +31,8 @@ from sklearn.preprocessing import StandardScaler
 @click.option(
     "--model",
     default="logreg",
-    type = click.Choice(['logreg', 'randomforest']),
-    show_default=True
+    type=click.Choice(["logreg", "randomforest"]),
+    show_default=True,
 )
 @click.option(
     "--use-scaler",
@@ -70,34 +71,53 @@ from sklearn.preprocessing import StandardScaler
     show_default=True,
 )
 def train(
-        dataset_path: Path, 
-        save_model_path: Path,
-        model: str,
-        use_scaler: bool,
-        max_iter: int,
-        logreg_c: int,
-        n_estimators: int,
-        max_depth: int,
-        random_state: int) -> None:
+    dataset_path: Path,
+    save_model_path: Path,
+    model: str,
+    use_scaler: bool,
+    max_iter: int,
+    logreg_c: int,
+    n_estimators: int,
+    max_depth: int,
+    random_state: int,
+) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
-    X = dataset.drop(['Id', 'Cover_Type'], axis=1)
-    y = dataset['Cover_Type']
+    X = dataset.drop(["Id", "Cover_Type"], axis=1)
+    y = dataset["Cover_Type"]
 
     steps = []
-    if (use_scaler):
-        steps.append(('scaler', StandardScaler()))
-    if (model == 'logreg'):
-        steps.append(('classifier', LogisticRegression(random_state=random_state, max_iter=max_iter, C=logreg_c)))
-    if (model == 'randomforest'):
-        steps.append(('classifier', RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=random_state)))
+    if use_scaler:
+        steps.append(("scaler", StandardScaler()))
+    if model == "logreg":
+        steps.append(
+            (
+                "classifier",
+                LogisticRegression(
+                    random_state=random_state, max_iter=max_iter, C=logreg_c
+                ),
+            )
+        )
+    if model == "randomforest":
+        steps.append(
+            (
+                "classifier",
+                RandomForestClassifier(
+                    n_estimators=n_estimators,
+                    max_depth=max_depth,
+                    random_state=random_state,
+                ),
+            )
+        )
 
     pipeline = Pipeline(steps)
 
-    cv_results = cross_validate(pipeline, X, y, cv=3, scoring=['accuracy', 'roc_auc_ovr', 'f1_micro'])
-    accuracy = cv_results['test_accuracy'].mean()
-    roc_auc = cv_results['test_roc_auc_ovr'].mean()
-    f1 = cv_results['test_f1_micro'].mean()
+    cv_results = cross_validate(
+        pipeline, X, y, cv=3, scoring=["accuracy", "roc_auc_ovr", "f1_micro"]
+    )
+    accuracy = cv_results["test_accuracy"].mean()
+    roc_auc = cv_results["test_roc_auc_ovr"].mean()
+    f1 = cv_results["test_f1_micro"].mean()
 
     mlflow.log_param("model", model)
     mlflow.log_param("max_iter", max_iter)
@@ -114,4 +134,4 @@ def train(
     click.echo(f"F1: {f1}")
 
     dump(pipeline, save_model_path)
-    click.echo(f'Model saved to {save_model_path}')
+    click.echo(f"Model saved to {save_model_path}")
