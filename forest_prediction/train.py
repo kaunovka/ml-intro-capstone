9e534bd7cd2b953f9786b4b9015d67c70db7cd52
kaunovka <kaunovka@gmail.com>
2022-05-06 11:24:25 +0300

    Add KFold evaluation to train

--- forest_prediction/train.py
+++ forest_prediction/train.py
@@ -1,25 +1,36 @@
 from pathlib import Path
+from joblib import dump
 
 import click
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
 
 @click.command()
 @click.option(
     "-d",
     "--dataset-path",
     default="data/train.csv",  
-    type=click.Path(exists=True, dir_okay=False, path_type=Path)
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True
 )
-def train(dataset_path: Path) -> None:
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+def train(dataset_path: Path, save_model_path: Path) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     X = dataset.drop(['Id', 'Cover_Type'], axis=1)
     y = dataset['Cover_Type']
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=.2, random_state=123)
-    clf = LogisticRegression(random_state=123).fit(X_train, y_train)
-    accuracy = accuracy_score(y_val, clf.predict(X_val))
-    click.echo(f"Accuracy: {accuracy}")
+    clf = LogisticRegression(random_state=123)
+    cv_results = cross_validate(clf, X, y, cv=3, scoring=['accuracy', 'roc_auc_ovr', 'f1_micro'])
+    click.echo(f"Accuracy: {cv_results['test_accuracy']}")
+    click.echo(f"ROC_AUC: {cv_results['test_roc_auc_ovr']}")
+    click.echo(f"F1: {cv_results['test_f1_micro']}")
+    dump(clf, save_model_path)
+    click.echo(f'Model saved to {save_model_path}')
